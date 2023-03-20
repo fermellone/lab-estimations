9e5404338106c21467677bd8529d4b0b5f76a70e
femellone <38559436+femellone@users.noreply.github.com>
2023-03-20 15:37:24 -0300

    debugging

--- src/routes/projects/[projectId]/epics/+page.server.ts
+++ src/routes/projects/[projectId]/epics/+page.server.ts
@@ -1,7 +1,7 @@
 import type { Epic } from '$lib/types';
 import { error, fail, type Actions, type ServerLoad } from '@sveltejs/kit';
 
-export const prerender = true;
+// export const prerender = true;
 
 export const load: ServerLoad = async ({ params }) => {
 	try {

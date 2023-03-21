ec7f6b939945b6fbb422bbcd58d63929084ae1da
forNerzul <sbeardman92@gmail.com>
2023-03-21 10:33:15 -0300

    debugging

--- src/routes/projects/[projectId]/requests/[requestId]/+error.svelte
+++ src/routes/projects/[projectId]/requests/[requestId]/+error.svelte
@@ -2,4 +2,13 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>holis</h1>
+{#await page}
+	<div>loading...</div>
+{:then page}
+	{#if page.error}
+		<div class="error">
+			<h1>{page.error.status}</h1>
+			<p>{page.error.message}</p>
+		</div>
+	{/if}
+{/await}
